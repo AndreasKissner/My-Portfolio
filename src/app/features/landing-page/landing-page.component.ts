4b096a2944d57e622d558caf4210b85a63931c22
Andreas Kissner <andikiss500@gmail.com>
2026-01-20 17:44:45 +0100

    feat: Add landing-page and section content

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { LandingContainerComponent } from './landing-container/landing-container.component';
+import { Component, input, signal } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/layout/header/header.component';
+import { FooterComponent } from '../../shared/components/layout/footer/footer.component';
+import { HeroComponent } from '../sections-content/hero/hero.component';
+import { AboutComponent } from '../sections-content/about/about.component';
+import { SkillComponent } from '../sections-content/skill/skill.component';
+import { PortfolioComponent } from '../sections-content/portfolio/portfolio.component';
+import { ReferenceComponent } from '../sections-content/reference/reference.component';
+import { ContactComponent } from '../sections-content/contact/contact.component';
+
+// Hier deine 6 Inhalts-Komponenten importieren
 
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LandingContainerComponent],
+  standalone: true,
+  imports: [
+    HeaderComponent, 
+    FooterComponent, 
+    HeroComponent, 
+    AboutComponent, 
+    SkillComponent, 
+    PortfolioComponent, 
+    ReferenceComponent, 
+    ContactComponent
+  ],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss',
+  styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
--- src/app/shared/components/layout/header/components/language-menue/language-menue.component.ts
+++ src/app/shared/components/layout/header/components/language-menue/language-menue.component.ts
@@ -8,25 +8,13 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrl: './language-menue.component.scss',
 })
 export class LanguageMenueComponent {
-
   private translate = inject(TranslateService);
-
-  // Ersetzt @Input() isDark: boolean = false;
   isDark = input<boolean>(false);
-
-  // Wenn die aktuelle Sprache Englisch ist, soll der DE-Button gezeigt werden.
-  // Also setzen wir isGerman auf true, damit das @if (isGerman) im HTML greift.
-  isGerman = this.translate.currentLang !== 'de'; 
+  isGerman = this.translate.currentLang !== 'de';
 
   toggleLanguage() {
-    // 1. Den Schalter für das Icon umlegen
     this.isGerman = !this.isGerman;
-    
-    // 2. Die Sprache basierend auf dem neuen Zustand wählen
-    // Wenn isGerman jetzt false ist, zeigen wir das EN-Icon und nutzen 'de'
-    // Wenn isGerman jetzt true ist, zeigen wir das DE-Icon und nutzen 'en'
     const lang = this.isGerman ? 'en' : 'de';
-    
     this.translate.use(lang);
     localStorage.setItem('lang', lang);
   }
