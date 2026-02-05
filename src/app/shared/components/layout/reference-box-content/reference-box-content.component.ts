ec50c21cb4340f8514d355bc49b356ccda81b243
Andreas Kissner <andikiss500@gmail.com>
2026-02-05 17:29:32 +0100

    feat: Portfolio section is now complete Ready with Responsive

---
+++ src/app/shared/components/layout/reference-box-content/reference-box-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reference-box-content',
+  imports: [],
+  templateUrl: './reference-box-content.component.html',
+  styleUrl: './reference-box-content.component.scss',
+})
+export class ReferenceBoxContentComponent {
+
+}
