11d6d0c07c512b6f4f972573c0b22967537fb65f
Andreas Kissner <andikiss500@gmail.com>
2026-02-06 08:02:53 +0100

    feat: Add Translation for Reference Section and link is now with hover

--- src/app/landing-page-content/sections-content/reference/reference.component.ts
+++ src/app/landing-page-content/sections-content/reference/reference.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ReferenceTilteComponent } from './reference-content/reference-tilte/reference-tilte.component';
 import { ReferenceBoxContentComponent } from '../../../shared/components/layout/reference-box-content/reference-box-content.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-reference',
-  imports: [ReferenceTilteComponent,ReferenceBoxContentComponent ],
+  imports: [ReferenceTilteComponent,ReferenceBoxContentComponent,TranslateModule ],
   templateUrl: './reference.component.html',
   styleUrl: './reference.component.scss',
 })
