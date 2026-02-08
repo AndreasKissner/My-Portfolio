f2f7e89e2e3c3691eab6af0e9096c6ddf677eb1d
Andreas Kissner <andikiss500@gmail.com>
2026-02-08 10:36:56 +0100

    feat: Sende Btn in Contact formular ist nor disable with scss logik

--- src/app/landing-page-content/sections-content/contact/contact-content/contact-email/contact-email.component.ts
+++ src/app/landing-page-content/sections-content/contact/contact-content/contact-email/contact-email.component.ts
@@ -2,13 +2,29 @@ import { Component } from '@angular/core';
 import { InputFieldsEmailComponent } from './input-fields-email/input-fields-email.component';
 import { CommonModule } from '@angular/common';
 import { BtnPrimaerComponent } from "../../../../../shared/components/layout/btn-primaer/btn-primaer.component";
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-email',
-  imports: [InputFieldsEmailComponent, CommonModule, BtnPrimaerComponent, BtnPrimaerComponent],
+  imports: [InputFieldsEmailComponent, CommonModule, BtnPrimaerComponent, BtnPrimaerComponent, FormsModule],
   templateUrl: './contact-email.component.html',
   styleUrl: './contact-email.component.scss',
 })
 export class ContactEmailComponent {
+contactData = {
+  name: '',
+  email: '',
+  message: '',
+  privacy: false
+};
 
+onSubmit(form: NgForm) {
+  if (form.valid) {
+    console.log('Formular-Daten:', this.contactData);
+    // Hier schickst du die Daten an deinen Server oder Email-Service
+    
+    // Nach dem Senden das Formular zurücksetzen:
+    form.resetForm();
+  }
+} 
 }
