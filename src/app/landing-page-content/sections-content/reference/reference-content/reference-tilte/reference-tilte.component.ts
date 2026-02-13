8ec84699df3b152cf9805c9f08b485cfd89072c1
Andreas Kissner <andikiss500@gmail.com>
2026-02-13 10:35:08 +0100

    feat: Make Contact for Desktop Version responsiv

--- src/app/landing-page-content/sections-content/reference/reference-content/reference-tilte/reference-tilte.component.ts
+++ src/app/landing-page-content/sections-content/reference/reference-content/reference-tilte/reference-tilte.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reference-tilte',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './reference-tilte.component.html',
   styleUrl: './reference-tilte.component.scss',
 })
