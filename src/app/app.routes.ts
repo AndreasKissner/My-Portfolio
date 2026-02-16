22d353ef4a883dceabf9e5a3a208b774ae34f0f0
Andreas Kissner <andikiss500@gmail.com>
2026-02-16 08:08:07 +0100

    feat: Add transalte in Imprint and legal plicy site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page-content/landing-page/landing-page.component';
 import { LegalNoticeComponent } from './shared/legal/legal-notice/legal-notice.component';
+import { PrivacyPoliceComponent } from './shared/legal/privacy/privacy-police/privacy-police.component';
 
 
 export const routes: Routes = [
@@ -9,5 +10,9 @@ export const routes: Routes = [
     },
     {
         path: 'legal-notice' , component : LegalNoticeComponent
-    }
+    },
+      {
+        path: 'privacy-policy' , component : PrivacyPoliceComponent
+    },
+
 ];
